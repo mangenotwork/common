d519a9052f5cd1fbaac9298776eca7023f86243f
ManGe <2912882908@qq.com>
2023-03-23 17:19:35 +0800

    add mq kafka

--- mq/kafka.go
+++ mq/kafka.go
@@ -1,8 +1,8 @@
 package mq
 
 import (
-	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/mangenotwork/common/log"
 	"sync"
 )
 
@@ -39,7 +39,7 @@ func (k *Kafka) Producer(data []byte) {
 	// 连接kafka
 	client, err := sarama.NewSyncProducer(k.Service, config)
 	if err != nil {
-		fmt.Println("Producer closed, err:", err)
+		log.Error("Producer closed, err:", err)
 		return
 	}
 	defer client.Close()
@@ -47,10 +47,10 @@ func (k *Kafka) Producer(data []byte) {
 	// 发送消息
 	pid, offset, err := client.SendMessage(msg)
 	if err != nil {
-		fmt.Println("send msg failed, err:", err)
+		log.Error("send msg failed, err:", err)
 		return
 	}
-	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+	log.InfoF("pid:%v offset:%v\n", pid, offset)
 }
 
 /*
@@ -72,25 +72,23 @@ func (k *Kafka) Consumer(ch chan []byte) {
 	var wg sync.WaitGroup
 	consumer, err := sarama.NewConsumer(k.Service, nil)
 	if err != nil {
-		fmt.Println("Failed to start consumer: %s", err)
+		log.ErrorF("Failed to start consumer: %s", err)
 		return
 	}
 	partitionList, err := consumer.Partitions("task-status-data") // 通过topic获取到所有的分区
 	if err != nil {
-		fmt.Println("Failed to get the list of partition: ", err)
+		log.Error("Failed to get the list of partition: ", err)
 		return
 	}
-	fmt.Println(partitionList)
-
 	for partition := range partitionList { // 遍历所有的分区
 		pc, err := consumer.ConsumePartition(k.Topic, int32(partition), sarama.OffsetNewest) // 针对每个分区创建一个分区消费者
 		if err != nil {
-			fmt.Println("Failed to start consumer for partition %d: %s\n", partition, err)
+			log.ErrorF("Failed to start consumer for partition %d: %s\n", partition, err)
 		}
 		wg.Add(1)
 		go func(sarama.PartitionConsumer) { // 为每个分区开一个go协程取值
 			for msg := range pc.Messages() { // 阻塞直到有值发送过来，然后再继续等待
-				fmt.Printf("Partition:%d, Offset:%d, key:%s, value:%s\n", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+				log.InfoF("Partition:%d, Offset:%d, key:%s, value:%s\n", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
 				ch <- msg.Value
 			}
 			defer pc.AsyncClose()
